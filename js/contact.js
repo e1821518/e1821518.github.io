834e0eabc1451f973dc8d2f979863456594b7ae1
e1821518 <68913749+e1821518@users.noreply.github.com>
2020-07-29 03:57:27 +0900

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,5 @@
 function check() {
-  if (contact.mail.value != "") {
+  if (contact.mail.value != "" && contact.lastname.value != "" &&contact.firtsname.value != "") {
     alert("ご連絡ありがとうございます。\n"+contact.mail.value + "にてお問い合わせを承りました。");
   } else {
     alert("必須項目が入力されていません。")
